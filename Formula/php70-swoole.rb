2546731a5e9c8b8d1b10dad3d9403b42877aa6b5
xuchao <85825770@qq.com>
2017-03-16 12:04:27 +0800

    php70-swoole: update 1.9.7-stable bottle.

--- Formula/php70-swoole.rb
+++ Formula/php70-swoole.rb
@@ -4,23 +4,16 @@ class Php70Swoole < AbstractPhp70Extension
   init
   desc "Event-driven asynchronous & concurrent networking engine for PHP."
   homepage "https://pecl.php.net/package/swoole"
-  url "https://github.com/swoole/swoole-src/archive/1.8.11-stable.tar.gz"
-  version "1.8.11-stable"
-  sha256 "cf2d9ba2c85f29c4a9f5e963878db27fdf552be7a05ca1709b6ad6f294e12b8e"
+  url "https://github.com/swoole/swoole-src/archive/v1.9.7.tar.gz"
+  version "v1.9.7"
+  sha256 "58401c3fb9e2fda642448ffd7c1547b4cf7155efe3322ffe517d99b3f95d736f"
   head "https://github.com/swoole/swoole-src.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "37bb6cb2b97c156aff4a3c9d55f785ace78c372d887e6c9c3e0a49c764be04e8" => :sierra
-    sha256 "e03a91d048da2898116ac49825dd736d49eede14fefc5b4306441398f2dab0f5" => :el_capitan
-    sha256 "29b7e4b70163670760d634982b7d890e612acc93c3f4cf15bb026fdfbcc321cb" => :yosemite
-  end
 
   def install
     ENV.universal_binary if build.universal?
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
+    system "./configure", "--prefix=#{prefix}", "--enable-coroutine", phpconfig
     system "make"
     prefix.install "modules/swoole.so"
     write_config_file if build.with? "config-file"
